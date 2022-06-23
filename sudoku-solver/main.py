65facd0c9097bb362999a7b20fb481d3a7def716
Radovan Blaha <kedar.blaha@gmail.com>
2022-06-23 22:35:52 +0200

    Sudoku Input read

--- sudoku-solver/main.py
+++ sudoku-solver/main.py
@@ -1,11 +1,19 @@
-def load_from_file():
-    pass
+class Sudoku:
+    def __init__(self, lines):
+        self.lines = [[int(char) if char != '_' else 0 for char in line.split()] for line in lines]
+
+
+def sudoku_from_file(file):
+    if not validate_file(file):
+        return None
+    with open(file) as my_file:
+        return Sudoku(my_file.readlines())
 
 
 def validate_file(file):
     valid = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '_'}
-    with open(file) as sudoku:
-        lines = sudoku.readlines()
+    with open(file) as my_file:
+        lines = my_file.readlines()
         if len(lines) != 9:
             return False
         for line in lines:
@@ -14,8 +22,9 @@ def validate_file(file):
             for char in line.split():
                 if char not in valid:
                     return False
-
     return True
 
 
-print(validate_file("input"))
+sudoku = sudoku_from_file("input")
+print("end")
+
